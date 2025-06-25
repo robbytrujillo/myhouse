ab94d039a3043f98bb7c78f2545deed9f29ec5e0
robbytrujillo <robbyikusuma88@gmail.com>
2025-06-25 14:09:30 +0700

    Introduction My Houses

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+const btnHamburger = document.querySelector(".hamburger");
+btnHamburger.addEventListener("click", () => {
+    btnHamburger.classList.toggle("is-active");
+});
+
+const menu = document.querySelector(".menu");
