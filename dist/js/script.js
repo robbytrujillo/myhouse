e798b798d69aa2097ce175c958f53f5cc52b1bfa
robbytrujillo <robbyikusuma88@gmail.com>
2025-07-04 15:26:08 +0700

    Introduction My Houses

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,4 @@
+// Menu Start
 const btnHamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 
@@ -5,3 +6,22 @@ btnHamburger.addEventListener("click", () => {
     btnHamburger.classList.toggle("is-active");
     menu.classList.toggle("menu-active");
 });
+// Menu End
+
+// Feedback Start
+const loadMore = document.querySelector(".load-more-btn");
+let jumlahItem = 3;
+
+loadMore.addEventListener("click", () => {
+    // console.log("load more");
+    let boxes = [...document.querySelectorAll(".feedback .feedback-box .box")];
+    for (let i = jumlahItem; i < jumlahItem + 3; i++) {
+        boxes[i].style.display = "inline-block";
+    }
+
+    jumlahItem += 3;
+    if (jumlahItem >= boxes.length) {
+        loadMore.style.display = "none";
+    }
+})
+// Feedback End
